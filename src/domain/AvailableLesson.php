e79035bc36e3efe4b50cca6b751aaf3f7e5f20c7
mtmtkj <mtmt101@usingstd.com>
2019-12-28 14:52:27 +0900

    Make AvailableLesson::getAvailability

---
+++ src/domain/AvailableLesson.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace YogaStudioReservationModeling;
+
+class AvailableLesson
+{
+    /**
+     * 枠がない: 0
+     * 3枠以下: 1
+     * それ以外: 2
+     * @param int $remaining
+     * @return int
+     */
+    public function getAvailability(int $remaining): int
+    {
+        if ($remaining === 0) {
+            return 0;
+        }
+        if ($remaining === 3) {
+            return 1;
+        }
+        return 2;
+    }
+}
---
+++ test/domain/AvailableLessonTest.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace YogaStudioReservationModeling\Test;
+
+use PHPUnit\Framework\TestCase;
+use YogaStudioReservationModeling\AvailableLesson;
+
+class AvailableLessonTest extends TestCase
+{
+    /**
+     * @param int $remaining
+     * @param int $expected
+     * @dataProvider dataCheckAvailableLesson
+     */
+    public function testCheckAvailableLesson(int $remaining, int $expected)
+    {
+        $lesson = new AvailableLesson();
+        $value = $lesson->getAvailability($remaining);
+        $this->assertSame($expected, $value);
+    }
+
+    public function dataCheckAvailableLesson()
+    {
+        return [
+            '残り枠がない' => [
+                'remaining' => 0,
+                'expected' => 0,
+            ],
+            '3枠以下' => [
+                'remaining' => 3,
+                'expected' => 1,
+            ],
+            '3枠より多くある' => [
+                'remaining' => 4,
+                'expected' => 2,
+            ],
+        ];
+    }
+}
