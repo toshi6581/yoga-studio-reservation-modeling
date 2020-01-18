8272fbb6f7d438938c5bcb2d4136617204e6d4b9
Toshi <toshi3arai3@gmail.com>
2020-01-18 19:48:21 +0900

    set property and construct in LessonScheduleSlot class

--- src/domain/LessonScheduleSlot.php
+++ src/domain/LessonScheduleSlot.php
@@ -1,10 +1,38 @@
 <?php
 
+//require_once('AlailableLesson.php); and need to extend class?
+//to use getAvailability?
+
 declare(strict_types=1);
 
 namespace YogaStudioReservationModeling;
 
 class LessonScheduleSlot
 {
-    // test;
+    // レッスン（スタジオと講師とレッスンの内容） → 
+    // private $studio;
+    // private $teacher;
+    //今は$lessonだけでOK。こんな感じで値を取る$lesson->studio->maxAttendee 
+    private $lesson;
+
+    //レッスン枠（何曜日の何時からやります。人数、最大人数（＝スタジオの最大人数と同じ））
+    private $startLesson;
+    private $endLesson;
+    private $attendee;
+    private $maxAttendee;
+
+    public function __construct($lesson, $startLesson, $endLesson, $attendee, $maxAttendee)
+    {
+        $this->lesson = $lesson;
+        $this->startLesson = $startLesson;
+        $this->endLesson = $endLesson;
+        $this->attendee = $attendee;
+        $this->maxAttendee = $maxAttendee;
+    }
+
+    public function getAvailability()
+    {
+        //
+        return $this->something;
+    }
 }
